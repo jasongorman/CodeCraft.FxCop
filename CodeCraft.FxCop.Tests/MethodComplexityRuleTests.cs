31d3dfc1ea7da36fb1fc342d26416b267f37e4d3
Jason Gorman <jason.gorman@codemanship.com>
2017-08-07 13:35:46 +0100

    Added method complexity rule
    Will allow 2 if branches, 1 loop, 1 loop + 1 if, and only a single
    default switch case.

---
+++ CodeCraft.FxCop.Tests/MethodComplexityRuleTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeCraft.FxCop.FeatureEnvy;
+using CodeCraft.FxCop.LongParameterList;
+using CodeCraft.FxCop.MethodComplexity;
+using Microsoft.FxCop.Sdk;
+using NUnit.Framework;
+
+namespace CodeCraft.FxCop.Tests
+{
+    [TestFixture]
+    public class MethodComplexityRuleTests
+    {
+        [TestCase("MethodWithTwoBranches", 0)]
+        [TestCase("MethodWithOneLoop", 0)]
+        [TestCase("OnlySwitchAllowed", 0)]
+        [TestCase("MethodWithThreeBranches", 1)]
+        [TestCase("MethodWithTwoLoops", 1)]
+        public void MethodsWithMoreThanThreeBranchesBreakRule(string methodName, int expectedProblemCount)
+        {
+            MethodComplexityRule rule = new MethodComplexityRule();
+            var memberToCheck = GetMemberToCheck(methodName);
+            rule.Check(memberToCheck);
+            Assert.AreEqual(expectedProblemCount, rule.Problems.Count);
+        }
+
+        private Member GetMemberToCheck(string methodName)
+        {
+            Type type = typeof (ClassD);
+            AssemblyNode assembly = AssemblyNode.GetAssembly(type.Module.Assembly.Location);
+            TypeNode typeNode = assembly.GetType(Identifier.For(type.Namespace), Identifier.For(type.Name));
+            Member methodToCheck = GetMethodByName(typeNode, methodName);
+            return methodToCheck;
+        }
+
+        private Member GetMethodByName(TypeNode typeNode, string methodName)
+        {
+            return typeNode.Members.FirstOrDefault(member => member.Name.Name == methodName);
+        }
+    }
+
+    internal class ClassD
+    {
+        void MethodWithTwoBranches()
+        {
+            int x = 0;
+
+            if (x > -1)
+            {
+                if (x < 1)
+                {
+                    x++;
+                }
+            }
+        }
+
+        void MethodWithThreeBranches()
+        {
+            int x = 0;
+
+            if (x > -1)
+            {
+                if (x < 1)
+                {
+                    if (x == 0)
+                    {
+                        x++;
+                    }
+                }
+            }
+        }
+
+        void MethodWithOneLoop()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+       
+            }
+        }
+
+        void MethodWithTwoLoops()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    
+                }
+            }
+        }
+
+        void OnlySwitchAllowed()
+        {
+            int x = 0;
+            switch (x)
+            {
+                default:
+                    break;
+            }
+        }
+    }
+
+}
---
+++ CodeCraft.FxCop/MethodComplexity/MethodComplexityRule.cs
@@ -0,0 +1,48 @@
+﻿using CodeCraft.FxCop.LongParameterList;
+using Microsoft.FxCop.Sdk;
+
+namespace CodeCraft.FxCop.MethodComplexity
+{
+    public class MethodComplexityRule : BaseIntrospectionRule
+    {
+        private int _branches = 0;
+
+        public MethodComplexityRule()
+            : base(
+                "MethodComplexityRule", "CodeCraft.FxCop.MethodComplexity.MethodComplexityRuleMetadata",
+                typeof (MethodComplexityRule).Assembly)
+        {
+        }
+
+        public override ProblemCollection Check(Member member)
+        {
+            Method method = member as Method;
+
+            if (method == null)
+            {
+                return Problems;
+            }
+
+            VisitStatements(method.Body.Statements);
+
+            if (_branches > 2)
+            {
+                string[] resolutionParams = {method.FullName};
+                Problems.Add(new Problem(new Resolution("Method {0} has too many branches", resolutionParams)));
+            }
+            return this.Problems;
+        }
+
+        public override void VisitBranch(Branch branch)
+        {
+            _branches++;
+            base.VisitBranch(branch);
+        }
+
+        public override void VisitSwitchInstruction(SwitchInstruction switchInstruction)
+        {
+            _branches++;
+            base.VisitSwitchInstruction(switchInstruction);
+        }
+    }
+}
